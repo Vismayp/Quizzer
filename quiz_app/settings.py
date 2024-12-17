3f2785941bc585ec88e234d5aac3641fe12651ac
Vismayp25205703 <Vismayp2520703@gmail.com>
2024-12-17 12:41:58 +0530

    Postgres

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'quiz_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'XjDxdYfZDMnYRQKtCRNaCIdsPLUkpDFs',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '13430'
     }
 }
 
